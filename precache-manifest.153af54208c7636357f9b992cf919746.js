12e78dcd5a39e9553f4235617e1fb83f96d59056
sklizkiygad <dante.spardo@mail.ru>
2022-06-13 18:52:10 +0300

    react

---
+++ precache-manifest.153af54208c7636357f9b992cf919746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a0ccf57facaf553eca",
+    "url": "/static/css/main.ef8fbf38.chunk.css"
+  },
+  {
+    "revision": "02a0ccf57facaf553eca",
+    "url": "/static/js/main.02a0ccf5.chunk.js"
+  },
+  {
+    "revision": "44b9111c3936b4cf2123",
+    "url": "/static/js/1.44b9111c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a73e677ca1c79d44ebe18ec229cb83b",
+    "url": "/index.html"
+  }
+];
